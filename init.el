3ac8decac6a13b0536db546f4856a7e9e9a41eaf
Jianming Qu <jianming.qu@rea-group.com>
2016-08-14 14:34:33 +0800

    add helm

--- init.el
+++ init.el
@@ -1,8 +1,22 @@
 ;; Cask loading
 (require 'cask "/usr/local/Cellar/cask/0.7.2_1/cask.el")
 (cask-initialize)
-
+(require 'pallet)
+(pallet-mode t)
 
 ;; Org config loading
 (require 'org)
+(setq org-confirm-babel-evaluate nil)
 (org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
